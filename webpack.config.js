5ae3d0993e4631188fbb251780580feb6390b0da
Richard Ma <rich.maaa@gmail.com>
2018-08-20 16:26:16 -0700

    Fix file structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,8 @@
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/canvas.js',
+    entry: "./lib/asteroids.js",
     output: {
-        path: __dirname + '/dist/',
-        filename: './js/canvas.bundle.js'
+        filename: "./lib/bundle.js"
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
-            }
-        ]
-    },
-    plugins: [
-        new BrowserSyncPlugin({
-            host: 'localhost',
-            port: 3000,
-            server: { baseDir: ['dist'] },
-            files: ['./dist/*']
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'src/index.html'
-        })
-    ],
-    watch: true,
-    devtool: 'source-map'
-}
+    devtool: 'source-map',
+};
